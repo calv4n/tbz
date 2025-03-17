889fa825eeeb2579f256c0d552bfd6102383d9b8
Calvin <calvin.pfrender@edu.tbz.ch>
2025-03-17 08:08:02 +0100

    fred

--- M183/OWASP-Auftrag/project/routes/auth.py
+++ M183/OWASP-Auftrag/project/routes/auth.py
@@ -7,3 +7,7 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/generate_token')
 def generate_token():
     return {"token": secrets.token_hex(32)}
+
+print(generate_token())
+
+# http://127.0.0.1:5000/auth/generate_token
